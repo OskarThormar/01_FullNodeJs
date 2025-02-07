68104c6f4d3664b4964f015a90f1b5f1303a6f73
OskarThormar <osth0002@stud.kea.dk>
2025-02-07 10:25:08 +0100

    build tools

--- 01._Miscellaneous/functions.js
+++ 01._Miscellaneous/functions.js
@@ -4,7 +4,7 @@
 console.log(getRandomInt(0, 10));
 
 function getRandomInt(min, max) {
-	return Math.flor(Math.random() * (max + 1 - min) - min);
+	return Math.floor(Math.random() * (max + 1 - min) - min);
 }
 
 const getRandomIntAnonymousFunction = function (min, max) {
@@ -15,14 +15,32 @@ const getRandomIntArrowFunction =   (min, max) => {
 	return Math.floor(Math.random() * (max + 1 - min) - min);
 };
 
+// action er en callback function
+// a callback function is a functio nthat is passed a a parameter to another function
+// possibly with the aim of calling it later
+
+                        // name: string  action: function
 function genericPerformer(name, action) {
 	return action(name);
-}
+};
 
 //  Lasse coding
-const codingAction = (name) => `${name} likes coding`;
+const codingAction = (name) => {
+    return `${name} likes coding`;
+};
 
-console.log(genericPerformer("Lasse", condingAction()));
+console.log(genericPerformer("Lasse", codingAction));
 
+// Andreas sleeping
+
+function sleepingAction(name) {
+    return `${name} loves sleeping`;
+}
+
+console.log(genericPerformer("Andreas", sleepingAction));
+
+const boxingAction = (name) => { return `${name} owns at boxing`};
+
+console.log(genericPerformer("Tara", boxingAction));
 
 
